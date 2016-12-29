7de10ac4cf8ca5b5c14e1929e366a91891490c3e
Kris Lamb <lamb.kris@gmail.com>
2016-12-28 19:27:01 -0500

    don't recall...

--- src/TestOpenCVApp.cpp
+++ src/TestOpenCVApp.cpp
@@ -11,6 +11,7 @@
 #include "Sharpen.h"
 #include "Sobel.h"
 #include "Threshold.h"
+#include "Tracking.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -23,12 +24,19 @@ public:
 	void draw() override;
 	void resize() override;
 
+	void mouseMove(MouseEvent event) override {
+		m_mouse_loc = event.getPos();
+	}
+
 private:
 	CaptureRef			mCapture;
 	gl::TextureRef		mTexture;
 	std::vector<std::unique_ptr<IRenderMode>> m_modes;
 	std::mutex render_mode_mutex;
 	decltype(m_modes)::iterator current_render_mode;
+	glm::ivec2 m_mouse_loc;
+
+	cv::Scalar GetColorAtPosition(glm::ivec2 position);
 };
 
 void ocvCaptureApp::setup()
@@ -38,6 +46,7 @@ void ocvCaptureApp::setup()
 		m_modes.emplace_back(new Sharpen());
 		m_modes.emplace_back(new Sobel());
 		m_modes.emplace_back(new Threshold());
+		m_modes.emplace_back(new Tracking());
 
 		current_render_mode = m_modes.begin();
 
@@ -99,10 +108,30 @@ void ocvCaptureApp::draw()
 		Rectf drawRect(static_cast<float>(window_size.x), 0.0f, 0.0f, static_cast<float>(window_size.y));
 		gl::draw(mTexture, drawRect);
 	}
+
+	auto color = GetColorAtPosition(m_mouse_loc);
+	gl::drawString("RGB: " + std::to_string(color[0]) + " " + std::to_string(color[1]) + " " + std::to_string(color[2]), glm::vec2(0.0f, 0.0f), ColorA(1, 1, 1, 1));
+/*
+	>> > green = np.uint8([[[0, 255, 0]]])
+		>> > hsv_green = cv2.cvtColor(green, cv2.COLOR_BGR2HSV)
+		>> > print hsv_green*/
+
+	auto hsv_color = cv::Mat{};
+	cv::cvtColor(cv::Mat(color), hsv_color, cv::COLOR_BGR2HSV);
+
+	gl::drawString("HSV: ", glm::vec2(0.0f, 20.0f), ColorA(1, 1, 1, 1));
 }
 
 void ocvCaptureApp::resize()
 {
+}
+
+cv::Scalar ocvCaptureApp::GetColorAtPosition(glm::ivec2 position)
+{
+	unsigned char color_bytes[3];
+	gl::readPixels(position.x, position.y, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, color_bytes);
+
+	return cv::Scalar{ (double)color_bytes[0], (double)color_bytes[1], (double)color_bytes[2] };
 }
 
 CINDER_APP(ocvCaptureApp, RendererGl)
